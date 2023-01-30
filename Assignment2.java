8e36e47f24a54a0bac989ac2b4744e39b30c7dd6
vmgriest <vmgriest@oakland.edu>
2023-01-29 19:31:48 -0500

    renamed to Assignment2

--- Assignment2.java
+++ Assignment2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Assignment2 {
     public static void main(String[] args) throws Exception {
         float grade;
         char letter = 'A';
